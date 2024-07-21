f849e4dbf3d9a2eb9c104c012dd93cbae73c7c18
Piotr Adamski <piotr.a8888@gmail.com>
2024-07-21 12:51:48 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -38450,7 +38450,7 @@ var _reactRouterDom = require("react-router-dom");
 var _App = _interopRequireDefault(require("./App"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 var root = _client.default.createRoot(document.getElementById("root"));
-root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_reactRouterDom.BrowserRouter, null, /*#__PURE__*/_react.default.createElement(_App.default, null))));
+root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_reactRouterDom.HashRouter, null, /*#__PURE__*/_react.default.createElement(_App.default, null))));
 },{"react":"../node_modules/react/index.js","react-dom/client":"../node_modules/react-dom/client.js","react-router-dom":"../node_modules/react-router-dom/dist/index.js","./App":"App.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -38476,7 +38476,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51288" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51728" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
