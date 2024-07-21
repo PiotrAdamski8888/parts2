aaf5262eab73f9a7a8b71086b226c558ef8c312f
Piotr Adamski <piotr.a8888@gmail.com>
2024-07-21 11:28:47 +0200

    Deploy test build

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import HomePage from "./pages/HomePage";
+// import Dashboard from "./pages/Dashboard";
+
+const App = () => {
+  return (
+    <div>App</div>
+    // <Router>
+    //   <div className="container fade-in">
+    //     <Routes>
+    //       <Route path="/" element={<HomePage />} />
+    //       <Route path="/dashboard/:role" element={<Dashboard />} />
+    //     </Routes>
+    //   </div>
+    // </Router>
+  );
+};
+
+export default App;
